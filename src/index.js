2a18b2e419bb47c589595e9737805559d8678782
Grégory Paul <paulgreg@gmail.com>
2018-03-25 16:23:36 +0200

    Fix some images + let

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-var pokedex = require('../Pokemon-DB/pokedex.json')
+let pokedex = require('../Pokemon-DB/pokedex.json')
 
-function removeGender (name) {
-    return name.replace('♀', '').replace('♂', '')
+function getImgName(name) {
+    return name.replace('♀', '').replace('♂', '').replace('. ', '_').replace(' ', '_').replace('Jr.', 'Jr')
 }
 
-var ul = document.createElement('ul')
+let ul = document.createElement('ul')
 
-pokedex.map(pokemon => {
-    var li = document.createElement('li')
+pokedex.map((pokemon, idx) => {
+    let li = document.createElement('li')
     //console.log(pokemon)
-    li.innerHTML = `<img src="../../Pokemon-DB/img/${pokemon.id}${removeGender(pokemon.ename)}.png"><h2>${pokemon.id} ${pokemon.ename}</h2><p>${JSON.stringify(pokemon.base)}</p>`
+    li.innerHTML = `<img ${idx < 10 ? '' : 'data-'}src="../../Pokemon-DB/img/${pokemon.id}${getImgName(pokemon.ename)}.png"><h2>${pokemon.id} ${pokemon.ename}</h2><p>${JSON.stringify(pokemon.base)}</p>`
     ul.appendChild(li)
 })
 
